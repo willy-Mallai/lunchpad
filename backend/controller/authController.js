8eb01948379aeb04a25af7961c2a6493bc9e4df1
willy-Mallai <willybertsang@gmail.com>
2026-09-07 20:08:10 +0530

    Fix Nodemailer email bug

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -32,10 +32,10 @@ const register = async (req, res) => {
 
     // 5. Hash the password using bcrypt.hash()
     const hashedPassword = await bcrypt.hash(password, 10);
-    
+
     // 6. Generate a random 6-digit OTP
     const otp = Math.floor(100000 + Math.random() * 900000).toString();
-    
+
     // 7. Create a new user instance using your userModel and the hashed password
     const newUser = await userModel.create({
       name,
@@ -49,7 +49,7 @@ const register = async (req, res) => {
     const token = await jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: "7d",
     });
-    
+
     // 9. Set the token as an httpOnly cookie
     res.cookie("token", token, {
       httpOnly: true,
@@ -59,12 +59,8 @@ const register = async (req, res) => {
     });
 
     // 10. Attempt to send the OTP email automatically
-    try {
-      await sendVerifyOtpEmail(newUser.email, otp);
-    } catch (emailError) {
-      console.log("Email failed to send during registration, but user was created:", emailError.message);
-      // We don't throw here because we still want the registration to succeed.
-    }
+
+    await sendVerifyOtpEmail(newUser.email, otp);
 
     // 11. Return a success response
     res.status(201).json({
